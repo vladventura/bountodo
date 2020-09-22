1c8e16d0c55498f055a57f08a4704804190ddd6f
Vlad Ventura <vladventura@outlook.es>
2020-09-22 14:48:03 -0400

    Adding the rarity picker here

--- components/MainView/index.js
+++ components/MainView/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import { View } from "react-native";
 import InputBoxes from "../InputBoxes";
 import Todos from "../Todos";
+import RarityPicker from "../RarityPicker";
 const MainView = () => {
   return (
     <View>
       <InputBoxes />
+      <RarityPicker />
       <Todos />
     </View>
   );
