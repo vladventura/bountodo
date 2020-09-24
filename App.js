714a959bcf75594e0f6366bb36b9148a53e4631a
Vlad Ventura <vladventura@outlook.es>
2020-09-24 11:14:48 -0400

    Adding react router and laying out how the app would look like

--- App.js
+++ App.js
@@ -6,12 +6,16 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./store/reducers";
 
+import { NativeRouter, Route } from "react-router-native";
+
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default function App() {
   return (
     <Provider store={store}>
-      <MainView />
+      <NativeRouter>
+        <Route exact path="/" component={MainView} />
+      </NativeRouter>
     </Provider>
   );
 }
